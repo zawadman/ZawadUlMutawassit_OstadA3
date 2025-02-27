df77183feb17dc636ee4eb4ea81097d65a7cd4de
Zawad Mutawassit <zawadmann@gmail.com>
2025-02-27 04:44:24 -0330

    |||||>------| 5% progress

---
+++ powerOf5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool isPowerOf5(int n)
+{
+    
+}
+
+
+int main()
+{
+    int n = 55;
+    if (isPowerOf5(n))
+    {
+        cout<<"True"<<"\n";
+    }
+    else
+    {
+        cout<<"False"<<"\n";
+    }
+    return 0;
+}
