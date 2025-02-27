aaea349a5191cfeb2b08388a860cfdab8b082c8c
Zawad Mutawassit <zawadmann@gmail.com>
2025-02-27 04:52:50 -0330

    ||||||||||||||||| 100% done

--- powerOf5.cpp
+++ powerOf5.cpp
@@ -3,15 +3,35 @@
 using namespace std;
 
 
+int isPowerOf5Checker(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    if (n % 5 != 0)
+    {
+        return 0;
+    }
+    return isPowerOf5Checker(n/5);
+}
+
 bool isPowerOf5(int n)
 {
-    
+    if (isPowerOf5Checker(n) == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 
 int main()
 {
-    int n = 55;
+    int n = 125;
     if (isPowerOf5(n))
     {
         cout<<"True"<<"\n";
